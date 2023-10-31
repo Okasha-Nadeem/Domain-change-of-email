09463931e01e54a588bc5498f8a49b34d575de37
Muhammad Zain Zameer <137919776+Zain-Zameer@users.noreply.github.com>
2023-10-31 17:47:17 +0000

    replaced our doman to given Domain for more clarity

--- emailgenerator.py
+++ emailgenerator.py
@@ -1,9 +1,9 @@
 #Script to update domain on every email in a company demo
 
 # Function that will replace old email with new one
-def assignNewDomain(email,old_Domain,new_Domain):
-  if "@" + old_Domain in email:
-    Index = email.index("@" + old_Domain)
+def assignNewDomain(email,givenDomain,new_Domain):
+  if "@" + givenDomain in email:
+    Index = email.index("@" + givenDomain)
     newEmail = email[:Index] + "@" + new_Domain
     return newEmail
   return email
@@ -12,12 +12,12 @@ def assignNewDomain(email,old_Domain,new_Domain):
 #Input Email
 email = input("Enter Email: ")
 #Input Old Domain
-old_Domain = input("Enter old domain: ")
+givenDomain = input("Enter Given domain: ")
 #Input New Domain
 new_Domain = input("Enter new domain: ")
 
 #Updating using function
-update = assignNewDomain(email,old_Domain,new_Domain)
+update = assignNewDomain(email,givenDomain,new_Domain)
 print(update)
 
 #The End
